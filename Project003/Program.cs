6cfa219afafb6e4293e20c7f294658469a94df60
Mikhail <Gusev_74@mail.ru>
2022-08-06 02:32:38 +0300

    add Project003 Задача дни недели

---
+++ Project003/Program.cs
@@ -0,0 +1,31 @@
+﻿int DayWeek;
+Console.Write("Введите число дня недели: "); 
+DayWeek = Convert.ToInt32 (Console.ReadLine());
+if (DayWeek == 1)
+{
+    Console.WriteLine("Понедельник");      
+}
+if (DayWeek == 2)
+{
+   Console.WriteLine("Вторник"); 
+}
+if (DayWeek == 3)
+{
+   Console.WriteLine("Среда"); 
+}
+if (DayWeek == 4)
+{
+   Console.WriteLine("Четверг"); 
+}
+if (DayWeek == 5)
+{
+   Console.WriteLine("Пятница"); 
+}
+if (DayWeek == 6)
+{
+   Console.WriteLine("Суббота"); 
+}
+if (DayWeek == 7)
+{
+   Console.WriteLine("Воскресенье"); 
+}
