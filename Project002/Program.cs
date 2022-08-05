ca3df57f4d184f959f74870ce6b6d593c3781548
Mikhail <Gusev_74@mail.ru>
2022-08-06 02:24:11 +0300

    add Project002 Задача на квадрат числа

---
+++ Project002/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Приветствую Вас");
+int a, b;
+Console.WriteLine("Введите первое число: ");
+a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+b = Convert.ToInt32(Console.ReadLine());
+if (a == b * b)
+{
+    Console.WriteLine("Первое, указанное Вами число является квадратом второго");
+}
+else
+{
+    Console.WriteLine("Первое, указанное Вами число не является квадратом второго");
+}
+    
