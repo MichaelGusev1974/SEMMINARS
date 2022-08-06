29424558f15e669af6e23b39c1b0efc0f34df386
Mikhail <Gusev_74@mail.ru>
2022-08-07 00:02:14 +0300

    В  DZ003

---
+++ DZ004/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите  число: ");
+int N;
+N = Convert.ToInt32(Console.ReadLine());
+while (N > 1)
+{
+    if (N%2 == 0)
+    {
+    Console.Write(N);
+    Console.Write(" ");
+    }
+N = N - 1;
+}
+
