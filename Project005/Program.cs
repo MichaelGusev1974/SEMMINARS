28a7c9e3a26070f804f0e527e715fcc548da459d
Mikhail <Gusev_74@mail.ru>
2022-08-06 05:25:42 +0300

    add Project005 Задача про число N

---
+++ Project005/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число");
+int number;
+number = Convert.ToInt32(Console.ReadLine());
+int a = 0 - number;
+while (a <= number)
+{
+    Console.WriteLine(a);
+    a++;
+}
+
+    
+
+
