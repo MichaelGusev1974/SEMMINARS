22ed85502aad21d8f4a373aa991945ea8a8e35ec
Mikhail <Gusev_74@mail.ru>
2022-08-06 16:56:29 +0300

    add DZ003

---
+++ DZ003/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите  число: ");
+int a;
+int n;
+a = Convert.ToInt32(Console.ReadLine());
+n = a%2;
+if (n == 0 )
+{
+    Console.Write(a);
+    Console.WriteLine(" - да, число является четным "); 
+}
+else
+{
+    Console.Write(a);
+    Console.WriteLine(" - нет, число не является четным ");
+}
