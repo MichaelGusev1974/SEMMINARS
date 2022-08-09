e75688475134a8eb5f182179a9aab0a4065d1ed3
Mikhail <Gusev_74@mail.ru>
2022-08-10 00:14:41 +0300

    add Project006

---
+++ Project006/Program.cs
@@ -0,0 +1,6 @@
+﻿int number1 = new Random().Next(100,1000);
+Console.WriteLine(number1);
+int number2 = number1 / 100;
+int number3 = number1%10;
+Console.Write(number2);
+Console.WriteLine(number3);
