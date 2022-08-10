17889645666f665132f210dadf5871d0b720b577
Mikhail <Gusev_74@mail.ru>
2022-08-10 14:25:00 +0300

    add Project009

---
+++ Project009/Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int a = 7;
+int b = 23;
+if (((num%a) == 0) && ((num%b) ==0))
+{
+ Console.WriteLine($"Число {num} кратно одновременно числу {a} и числу {b}"); 
+}  
+else
+{
+  Console.WriteLine($"Число {num} не кратно одновременно числу {a} и числу {b}");   
+}
