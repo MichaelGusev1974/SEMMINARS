23ce2a81d22dd23369005b0a1a4a243c3e57769b
Mikhail <Gusev_74@mail.ru>
2022-08-10 22:27:20 +0300

    add Project/Program.cs

--- Project009/Program.cs
+++ Project009/Program.cs
@@ -3,7 +3,7 @@ Console.WriteLine("Введите число: ");
 int num = Convert.ToInt32(Console.ReadLine());
 int a = 7;
 int b = 23;
-if (((num%a) == 0) && ((num%b) ==0))
+if (((num%a) == 0) & ((num%b) ==0))
 {
  Console.WriteLine($"Число {num} кратно одновременно числу {a} и числу {b}"); 
 }  
