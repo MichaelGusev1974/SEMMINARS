be31bd3acdf6592fda7a9893a8ca65865c89939e
Mikhail <Gusev_74@mail.ru>
2022-08-10 03:20:12 +0300

    add Project008

---
+++ Project008/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1%num2 == 0)
+{
+  Console.Write(num2);
+  Console.WriteLine(" число кратное"); 
+}  
+else
+{
+    Console.Write("Остаток: ");
+    Console.Write(num1%num2);
+    Console.WriteLine(" число не кратное"); 
+} 
