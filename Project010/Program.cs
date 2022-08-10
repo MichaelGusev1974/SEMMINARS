cc62ebc113ebcbe48e214d414d3e3ce55ff2833e
Mikhail <Gusev_74@mail.ru>
2022-08-10 14:25:35 +0300

    add Project010

---
+++ Project010/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1*num1 == num2) 
+{
+  Console.WriteLine($" число {num2} является квадратом {num1}"); 
+}  
+else if(num2*num2 == num1) 
+{
+  Console.Write($"число {num1}  является квадратом {num2} ");
+}     
+else
+{
+  Console.Write($"число {num1} и {num2} не является квадратами в отношении друг друга");
+}
