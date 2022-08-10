f7f337d1b0bdd01092a249d0d68763e07431e8a3
Mikhail <Gusev_74@mail.ru>
2022-08-10 18:07:20 +0300

    add DZ005 - доработал код

--- DZ005/Program.cs
+++ DZ005/Program.cs
@@ -1,7 +1,10 @@
 ﻿Console.WriteLine("Введите трехзначное число: ");
 int num = Convert.ToInt32(Console.ReadLine());
-
+if ((num < 99) & (num > -99)) Console.WriteLine( "-это не трехзначное число");
+if (num < 0) num = num * -1;
+if ((num >= 100) & (num < 1000))
+{
 num = num % 100;
 num = num / 10;
-
 Console.WriteLine(num);
+}
