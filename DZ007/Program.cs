4364b25ed411e524b67fe76a18fac8bb85165710
Mikhail <Gusev_74@mail.ru>
2022-08-10 17:22:01 +0300

    add DZ007

---
+++ DZ007/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Введите число дня недели: "); 
+int num = Convert.ToInt32 (Console.ReadLine());
+if (num <= 5) Console.WriteLine("Нет рабочий день");
+// Console.WriteLine("Да выходной день");
+else if ((num > 5) & (num <=7)) Console.WriteLine("Да выходной день");
+else if (num > 7) Console.WriteLine("нет такого дня недели");
