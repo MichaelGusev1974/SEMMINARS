9ecbda62395c5ec435c0e189f1694d387310b833
Mikhail <Gusev_74@mail.ru>
2022-08-10 16:44:42 +0300

    add DZ006

---
+++ DZ006/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите любое число от в диапозоне - 10000 + 10000");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num < 0) num = num * -1;
+if (num < 100)
+{
+    Console.WriteLine($"у числа {num} третьей цифры нет");
+}
+if ((num >= 100) & (num < 1000))
+{
+    Console.WriteLine(num%10);
+} 
+if ((num >= 1000) & (num < 10000))
+{
+    num = num / 10;
+    Console.WriteLine(num%10);
+}    
