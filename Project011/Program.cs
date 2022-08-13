6b941a5965728acde19833799d6b4487b6cf5f68
Mikhail <Gusev_74@mail.ru>
2022-08-13 20:09:33 +0300

    add Project011

--- Project011/Program.cs
+++ Project011/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Вводим многозначное число.Необходимо узнать и сказать последовательность цифр этого 
+//числа при просмотре слева направо упорядочена по возрастанию или нет.
+//Например 1579 - да, 1427 - нет
+Console.Clear();
+Console.WriteLine("Введите число:  ");
+int num = Convert.ToInt32(Console.ReadLine());
+int index = 10;
+bool Check = false;
+while(num > 0)
+{
+  if(index > num%10)
+  {
+    Check = true;
+  }
+   else Check = false;
+    index = num%10;
+    num/=10;
+}
+if(Check)
+Console.WriteLine("Число  упорядочено"); 
+else Console.WriteLine("Число   не упорядочено");
