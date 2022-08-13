8e4319323b33859715f2abc80264ecbb4fe685f0
Mikhail <Gusev_74@mail.ru>
2022-08-13 19:12:25 +0300

    add file instruction in  Project015

--- Project015/Program.cs
+++ Project015/Program.cs
@@ -1,8 +1,4 @@
-﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу 
-//квадратов чисел от 1 до N.
-//5 -> 1, 4, 9, 16, 25.
-//2 -> 1,4
-Console.Clear();
+﻿Console.Clear();
 Console.WriteLine("Введите число:  ");
 int num = Convert.ToInt32(Console.ReadLine());
 int index = 1;
