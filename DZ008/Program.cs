82473f461a290338e9ea595ae0e5b231878694db
Mikhail <Gusev_74@mail.ru>
2022-08-13 21:26:41 +0300

    add DZ008

---
+++ DZ008/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел 
+//от 1 до N. Например: 3 -> 1, 8, 27; 5 -> 1, 8, 27, 64, 125
+Console.Clear();
+Console.WriteLine("Введите число:  ");
+int num = Convert.ToInt32(Console.ReadLine());
+int index = 1;
+while(index <= num)
+{
+    Console.Write($"{index*index*index} ");
+    index++;
+}
+Console.WriteLine();
