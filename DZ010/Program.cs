de999ddb0f7505ee0f18d38c544da61d58c22867
Mikhail <Gusev_74@mail.ru>
2022-08-13 23:08:26 +0300

    add DZ010

---
+++ DZ010/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является
+//ли оно палиндромом.Например: 14212 -> нет; 12821 -> да; 23432 -> да.
+Console.Clear();
+Console.WriteLine("Введите пятизначное число:  ");
+int num = Convert.ToInt32(Console.ReadLine());
+int index = 10000;
+bool Check = false;
+while(num > 0)
+{
+    index = num / index;
+    num = num % 10;
+    
+  if(index == num%10)
+  {
+    Check = true;
+  }
+   else Check = false;
+    index /= 10;
+    num /= 10;
+}
+if(Check)
+Console.WriteLine("Число  является палиндромом"); 
+else Console.WriteLine("Число   не является палиндромом");
