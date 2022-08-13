35d9878e7010f47b130f6e6cf64145ea50b51858
Mikhail <Gusev_74@mail.ru>
2022-08-13 15:15:38 +0300

    add Project013

---
+++ Project013/Program.cs
@@ -0,0 +1,30 @@
+﻿//  Напишите программу, которая по заданному номеру четверти, показывает диапазон 
+//возможных координат точек в этой четверти (x и y).
+Console.Clear();
+Console.WriteLine("Введите номер четверти ");
+
+int quterNumder = Convert.ToInt32(Console.ReadLine());
+
+
+
+if ((quterNumder < 0) || (quterNumder > 4))
+{
+ Console.WriteLine("Ввели неправильный номер четверти (от 1 до 4)");  
+ 
+}
+else if (quterNumder == 1)
+{
+  Console.WriteLine("Диапазон: x: от 0 до + infinity,  y: от 0 до + infinity");   
+}
+else if (quterNumder == 2)
+{
+  Console.WriteLine("Диапазон: x: от - infinity до 0,  y: от 0 до + infinity");    
+}
+else if (quterNumder == 3)
+{
+  Console.WriteLine("Диапазон: x: от  - infinity до 0,  y: от  - infinity до 0"); ;   
+}
+else if (quterNumder == 4)
+{
+  Console.WriteLine("Диапазон: x: от 0 до + infinity,  y: от  - infinity до 0");  
+}
