13871f1353334484255dd8c21c9d7f68f55ff95b
Mikhail <Gusev_74@mail.ru>
2022-08-17 02:00:38 +0300

    add project019

---
+++ Project019/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход число А, и выдает сумму чисел от 1 до А
+void SumOneToA(int a)
+{
+    int sum = 0;
+    for(int i = 1; i <= a; i++)
+    {
+        sum = sum + i;
+    }
+    Console.WriteLine(sum);
+}
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+SumOneToA(num);
+
+
+
+
+
+
+
