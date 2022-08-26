e8a6d1b8519008e6f23551c364c9d42bd10cfafa
Mikhail <Gusev_74@mail.ru>
2022-08-26 23:10:54 +0300

    add Project028

---
+++ Project028/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 48: Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aₘₙ = m+n. Выведите полученный массив на экран.
+// m = 3, n = 4.
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+int[,] array = new int [3,4];
+
+void PrintArray(int[,] arr)
+{
+    for(int i = 0; i < arr.GetLength(0); i++)
+    {
+    for(int j = 0; j < arr.GetLength(1); j++)
+        {
+         Console.Write($"{arr[i,j]} ");
+        }
+       Console.WriteLine();   
+    }
+} 
+void FillArray(int[,] arr)
+{
+    for(int i = 0; i < arr.GetLength(0); i++)
+    {
+   
+        for(int j = 0; j < arr.GetLength(1); j++)
+        {
+         arr[i,j] = i + j;
+        } 
+    }
+}
+FillArray(array);
+PrintArray(array);
