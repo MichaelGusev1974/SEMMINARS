c8249f6b9c8da907b4d99dae9b0c437b2d85dcf9
Mikhail <Gusev_74@mail.ru>
2022-08-27 23:03:32 +0300

    add modified  Project030

--- Project030/Program.cs
+++ Project030/Program.cs
@@ -20,22 +20,19 @@ void FillArray(int[,] arr)
 {
     for(int i = 0; i < arr.GetLength(0); i++)
     {
-   
         for(int j = 0; j < arr.GetLength(1); j++)
         {
 
          arr[i,j] = new Random().Next(1,10);   
-         //if(i%2 == 0 && j%2 == 0)  arr[i,j] = arr[i,j]*arr[i,j];
+         
         } 
-    }
-   
+    }  
 }
 void MultArray(int[,] arr)
 {
-    int result =0;
+    int result = 0;
     for(int i = 0; i < arr.GetLength(0); i++)
     {
-   
         for(int j = 0; j < arr.GetLength(1); j++)
         { 
          if(i == j) result = result + arr[i,j];
