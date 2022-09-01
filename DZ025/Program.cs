f49272f5ad98d9629eddd3cb77513593a9509e95
Mikhail <Gusev_74@mail.ru>
2022-09-01 21:45:59 +0300

    add modified DZ025 второй раз

--- DZ025/Program.cs
+++ DZ025/Program.cs
@@ -9,21 +9,17 @@ Console.Clear();
 var rand = new Random();
 void FillMatrix(int[,,] matrix)
 {
+    int value = 10;
     for(int i = 0; i < matrix.GetLength(0); i++)
     {
         for(int j = 0; j < matrix.GetLength(1); j++)
         {
             for(int k = 0; k < matrix.GetLength(2); k++)
             {
-                matrix[i, j, k] = new Random().Next(10,100);
-                int currentNumber = matrix[i, j, k];
-                int incomingNumber = 0;
-                if(incomingNumber == currentNumber)
-                break;
-                if(incomingNumber != currentNumber)
-                currentNumber = incomingNumber; 
+                
+                matrix[i, j, k] = value;
+                value = value + 1;
             }
-            
         }
     }
 }
