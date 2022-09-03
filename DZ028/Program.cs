4e3b7bbf83ded3716660d98cd0423251753f0d65
Mikhail <Gusev_74@mail.ru>
2022-09-03 16:47:04 +0300

    add DZ027, DZ028,DZ029

---
+++ DZ028/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+int NumbersRec( int m, int n) 
+ {
+     if(m <= n) return n + NumbersRec(m, n - 1); 
+     else 
+     {
+     return 0 ;
+     }
+ }
+ Console.WriteLine("Введите число m ");
+ int m = Convert.ToInt32(Console.ReadLine());
+ Console.WriteLine("Введите число n ");
+ int n = Convert.ToInt32(Console.ReadLine());
+ Console.WriteLine(NumbersRec(m, n));
---
+++ DZ029/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 29
+int AkkermunRec(int m, int n)
+{
+if(m == 0) return n + 1;
+else if((m > 0) && (n == 0)) return AkkermunRec(m - 1, 1);
+else if((m > 0) && (n > 0)) return AkkermunRec(m - 1, AkkermunRec(m, n - 1));
+else return n + 1;
+}
+
+Console.WriteLine("Введите число m ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число n ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(AkkermunRec(m, n));
